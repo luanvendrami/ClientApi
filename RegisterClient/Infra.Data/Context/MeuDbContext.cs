27348ca8a577f631f8e8f29b342fb3c720456735
luanvendrami <luan.vendrami@hotmail.com>
2021-08-07 15:44:05 -0300

    Configuração da startup para receber o banco de dados

---
+++ RegisterClient/Infra.Data/Context/MeuDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions<MeuDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
