74309281e63ba93eec4726e41d936bcdb6cd1e2f
luanvendrami <luan.vendrami@hotmail.com>
2021-08-08 20:45:12 -0300

    Criado A Dto conforme a tabela do banco de dados

---
+++ RegisterClient/Domain/Entidades/ClientDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entidades
+{
+    public class ClientDto
+    {
+        public int Id { get; set; }
+        public string NomeCompleto { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string  Cep { get; set; }
+    }
+}
