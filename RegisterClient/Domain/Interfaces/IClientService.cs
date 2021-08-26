1ff4924576ee1fdd31ef4ab9a4417cf65e855cfc
luanvendrami <luan.vendrami@hotmail.com>
2021-08-26 00:35:34 -0300

    Adicionado a interface do service do ClientService

---
+++ RegisterClient/Domain/Interfaces/IClientService.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IClientService
+    {
+        Task<IEnumerable<ClientDto>> GetClients();
+        Task<ClientDto> GetClient(int id);
+        Task<IEnumerable<ClientDto>> GetClientByNome(string nome);
+        Task CreateClient(ClientDto client);
+        Task UpdateClient(ClientDto client);
+        Task DeleteClient(ClientDto client);
+    }
+}
