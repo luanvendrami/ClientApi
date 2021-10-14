0a7e61e54366fb06fd8e1f520a04654aedb7eb9a
luanvendrami <luan.vendrami@hotmail.com>
2021-10-13 23:54:53 -0300

    Retirado classes que não eram usadas e feito nova classe de configureServices que vai ficar os metodos de injeção de dependência

---
+++ RegisterClient/Infra.CrossCuting/IoC/ConfigureServicesInjection.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Interfaces.ClienteInterface.Repositorio;
+using Domain.Interfaces.ClienteInterface.Service;
+using Infra.Data.Context;
+using Infra.Data.Repository.ClienteRepositorio;
+using Microsoft.Extensions.DependencyInjection;
+using Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.CrossCuting.IoC
+{
+    public class ConfigureServicesInjection
+    {
+        public static void AddInjectionContext(IServiceCollection services)
+        {
+            services.AddScoped<MeuDbContext>();
+        }
+
+        public static void AddInjectionRepositorio(IServiceCollection services)
+        {
+            services.AddScoped<IClienteRepositorio, ClienteRepositorio>();
+        }
+
+        public static void AddInjectionService(IServiceCollection services)
+        {
+            services.AddScoped<IClienteService, ClienteService>();
+        }
+    }
+}
